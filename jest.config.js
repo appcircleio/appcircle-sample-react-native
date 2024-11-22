e348f7c9349e23f8c166e9bc3cdc672db65a8422
boztopuz <boztopuz@gmail.com>
2024-11-22 12:31:18 +0300

    Added new jest config for coverage type

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,13 @@ module.exports = {
     '/node_modules/',
     '/e2e/', // Ignore e2e folder for unit tests
   ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}', // Adjust this to match your source directory
+    '!**/node_modules/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text', 'json'],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation)',
 
